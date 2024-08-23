675df59bee21d9ddf40a1b540b15ff1d363375ac
zohaib240 <zohaibjs123@gmail.com>
2024-08-22 19:49:14 -0700

    update jss files

--- register.js
+++ register.js
@@ -1,27 +1,65 @@
 import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
-import { auth } from "./config.js";
+import {
+  uploadBytes,
+  getDownloadURL,
+  ref,
+  getStorage
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-storage.js";
+import { auth , db} from "./config.js";
 
 
-
+const storage = getStorage()
 const name =document.querySelector('#name')
 const lastName =document.querySelector('#last_name')
 const email =document.querySelector('#Email')
 const password =document.querySelector('#Password')
+let myfile = document.querySelector("#myfile")
 const form =document.querySelector('#form')
 
 
-form.addEventListener('submit',(event)=>{
+form.addEventListener('submit',async (event)=>{
     event.preventDefault()
     console.log( email.value);
     console.log( password.value);
     createUserWithEmailAndPassword(auth, email.value, password.value)
-  .then((userCredential) => {
+  .then(async (userCredential) => {
     // Signed up 
     const user = userCredential.user;
     console.log(user)
-    window.location='login.html'
-    
+   
+    let file = myfile.files[0]
+    let url = null
+    if (file) {
+        url = await uploadFile(file, email.value)
+        console.log(url);
+    }
+    Swal.fire({
+      title: 'Success!',
+      text: 'Your account registered successfully!',
+      icon: 'success',
+      confirmButtonText: 'Login'
   })
+      .then((result) => {
+          if (result.isConfirmed) {
+              window.location = "./login.html";
+          }
+      });
+      
+        // add data into firestore database
+        try {
+          const docRef = await addDoc(collection(db, "users"), {
+              name: name.value,
+              last_name: lastName.value,
+              email: email.value,
+              uid: user.uid,
+              photoUrl: url
+          });
+          console.log("Document written with ID: ", docRef.id);
+      } catch (e) {
+          console.error("Error adding document: ", e);
+      }
+  })
+  
   .catch((error) => {
     const errorCode = error.code;
     const errorMessage = error.message;
@@ -35,3 +73,14 @@ form.addEventListener('submit',(event)=>{
 })
 
 
+// img to url convert function
+async function uploadFile(file, userEmail) {
+  const storageRef = ref(storage, userEmail);
+  try {
+      const uploadImg = await uploadBytes(storageRef, file);
+      const url = await getDownloadURL(uploadImg.ref);
+      return url;
+  } catch (error) {
+      console.error(error);
+      throw error;
+  }}
